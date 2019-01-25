f049b85f74353c4f742e825e3eaf4a980cf717a8
jemma <jemmanuelmg@hotmail.com>
2019-01-25 17:15:48 -0500

    POR FIN! ya se logró agregar la funcionalidad de trabajo sin conexión gracias al service worker. El problema era la lista de archivos enviados al caché. No se estaba haciendo correctamente. Ahora inicio, ayuda y registrarse funcionan sin conexion.

--- public/service-worker.js
+++ public/service-worker.js
@@ -4,62 +4,8 @@ var dataCacheName = 'Gesol:cache';
 var cacheName = 'Geso::cache';
 var filesToCache = [
   '/',
-  '/../resources/views/layouts/templateBasico.blade.php',
-  '/../resources/views/index.blade.php',
-
-  '/../resources/views/vistasUsuarios/create.blade.php',
-  '/../resources/views/vistasUsuarios/create.blade.php',
-  '/../resources/views/vistasUsuarios/edit.blade.php',
-  '/../resources/views/vistasUsuarios/indexUsuarios.blade.php',
-  '/../resources/views/vistasUsuarios/paginaAyuda.blade.php',
-
-  '/../resources/views/vistasSolicitudes/edit.blade.php',
-  '/../resources/views/vistasSolicitudes/indexSolicitudes.blade.php',
-  '/../resources/views/vistasSolicitudes/misSolicitudes.blade.php',
-  '/../resources/views/vistasSolicitudes/selectorSolicitudes.blade.php',
-
-  '/../resources/views/vistasRespuestas/edit.blade.php',
-  '/../resources/views/vistasRespuestas/indexRespuestas.blade.php',
-
-  '/../resources/views/correos/contacto.blade.php',
-  '/../resources/views/correos/plantillaConfirmaSol.blade.php',
-  '/../resources/views/correos/plantillaContacto.blade.php',
-  '/../resources/views/correos/plantillaRespuestaSol.blade.php',
-
-  '/../resources/views/metricas/grafico1.blade.php',
-  '/../resources/views/metricas/grafico2.blade.php',
-  '/../resources/views/metricas/grafico3.blade.php',
-
-  '/../resources/views/resetPassword/email.blade.php',
-  '/../resources/views/resetPassword/reset.blade.php',
-
-  '/../resources/views/notificaciones/mostrarErrorForm.blade.php',
-  '/../resources/views/notificaciones/mostrarMensajes.blade.php',
-
-  '/css/bootstrap.min.css',
-  '/css/styles-main-gesol.css',
-  '/fonts/fontawesome/css/all.min.css',
-
-  '/js/jquery-3.2.1.slim.min.js',
-  '/js/jquery-3.2.1.min.js',
-  '/js/popper.min.js',
-  '/js/bootstrap.min.js',
-  '/js/slick.min.js',
-  '/js/brand-carousell.js',
-  '/js/DataTables/media/js/jquery.dataTables.min.js',
-  '/js/slick.min.js',
-  '/js/brand-carousell.js',
-
-  '/images/slider-gesol-1.jpeg',
-  '/images/slider-gesol-2.jpeg',
-  '/images/slider-gesol-3.jpeg',
-  '/images/slider-gesol-4.jpeg',
-  '/images/gesol_logo_new2.png',
-  '/images/gesol-inicio-comunidad.jpeg',
-  '/images/gesol-inicio-esperar.jpeg',
-  '/images/gesol-inicio-solicitar.jpeg',
-  '/images/gesol-inicio-unirse.jpeg',
-  '/images/felices.jpeg'
+  'http://127.0.0.1:8000/ayuda'
+  
 ];
 
 
@@ -110,11 +56,10 @@ self.addEventListener('activate', function(e) {
 });
 
 
-
-
 /*
 self.addEventListener('fetch', function(e) {
 });
 */
+
 
 
