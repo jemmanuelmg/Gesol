0af2b21ffb2bebcb3c2c25ab56ca0e2c582b590d
jemma <jemmanuelmg@hotmail.com>
2019-07-20 13:28:57 -0500

    Actualizado paquete/dependencia google/protobuf la cual presentaba errores en el deployment a heroku. Volviendo a intentar deployment

--- resources/views/vistasUsuarios/create.blade.php
+++ resources/views/vistasUsuarios/create.blade.php
@@ -12,7 +12,7 @@ Registrarse en Gesol
 
 @section('contenido')
 
-<!-- CONTENEDOR PARA REGISTRARSE (este es un pequeño cambio para heroku) -->
+<!-- CONTENEDOR PARA REGISTRARSE (este es un pequeño cambio para heroku ) -->
 <div class="container">
 
 	<div class="jumbotron">
